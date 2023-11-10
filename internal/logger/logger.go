f85ee10a8f35770c00ace0d155e8ad6d38ee982f
Pavel <p.petrov94@mail.ru>
2023-11-10 20:48:26 +0300

    added logger and gzip

---
+++ internal/logger/logger.go
@@ -0,0 +1,89 @@
+package logger
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type ctxLogger struct{}
+
+var sugar zap.SugaredLogger
+
+func NewLogger() zap.SugaredLogger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar = *logger.Sugar()
+	return sugar
+}
+
+func ContextWithLogger(ctx context.Context, l *zap.SugaredLogger) context.Context {
+	return context.WithValue(ctx, ctxLogger{}, l)
+}
+
+func LoggerFromContext(ctx context.Context) *zap.SugaredLogger {
+	if l, ok := ctx.Value(ctxLogger{}).(*zap.SugaredLogger); ok {
+		return l
+	}
+	return &sugar
+}
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса.
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	logFn := func(rw http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ctx := ContextWithLogger(r.Context(), &sugar)
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: rw,
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r.WithContext(ctx))
+
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status,
+			"size", responseData.size,
+			"duration", duration,
+		)
+	}
+	return http.HandlerFunc(logFn)
+}
