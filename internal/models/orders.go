9b9a2704bf6f375cd81440e784fa3f02dc175ca6
Pavel <p.petrov94@mail.ru>
2023-11-28 23:08:36 +0300

    fix iter1

--- internal/models/orders.go
+++ internal/models/orders.go
@@ -11,7 +11,7 @@ const (
 
 type Orders struct {
 	UserID     int       `json:"-"`
-	Number     string    `json:"order_number"`
+	Number     string    `json:"number"`
 	Status     string    `json:"status"`
 	Accrual    float64   `json:"accrual,omitempty"`
 	UploadedAt time.Time `json:"uploaded_at"`
